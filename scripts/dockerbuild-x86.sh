fa9e79c642a0b2278d62fbd8bb30646cae9e4000
gli-mrunal <gli-mrunal@gmail.com>
2023-05-14 12:48:43 -0700

    added dockerfile that builds off a ros2 galactic image and use it to install OpenVINS on. The display_ros2.rviz is an rviz2 config file for euroc data set and open vins topics. created architecture folders for Dockerfiles where jetson-nano is for ARM arhictecture but being more specific because the Dockerfiles in there are based off of a jetson nano image. Also added a dockerbuild script to build for x86 docker images

---
+++ scripts/dockerbuild-x86.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONTAINER_NAME=$1
+DOCKERFILE=$2
+
+echo $CONTAINER_NAME
+echo $DOCKERFILE
+
+# You need docker buildx to use this
+# https://docs.docker.com/build/install-buildx/
+# add this to log the output of the docker build to a file: &> docker-build.log
+docker build --network=host --progress=plain -t $CONTAINER_NAME -f $DOCKERFILE .
