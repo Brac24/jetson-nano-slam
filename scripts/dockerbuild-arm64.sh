97a21d13e65163c10d3a7c64034c04fd6e44a71b
brac24-UbuntuHome <2brac4@gmail.com>
2023-07-12 19:19:02 -0700

    removing gtsam, libnabo, and libpointmatcher dependencies actually allows rtabmap to build without the relocation errors so maybe one of those dependencies has an issue. libpcl does not build though which is another issue

--- scripts/dockerbuild-arm64.sh
+++ scripts/dockerbuild-arm64.sh
@@ -9,4 +9,5 @@ echo $DOCKERFILE
 # You need docker buildx to use this
 # https://docs.docker.com/build/install-buildx/
 # add this to log the output of the docker build to a file: &> docker-build.log
+# add --no-cache flag to rebuild without using previous builds
 docker build --platform linux/arm64 --network=host --progress=plain -t $CONTAINER_NAME -f $DOCKERFILE .
