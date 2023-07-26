8598ad87a43750725bff47eaa5b08bd996101841
brac24-UbuntuHome <2brac4@gmail.com>
2023-07-25 20:45:58 -0700

    dockerbuild worked yesterday 07/24 building without gtsam, libnabo, and a couple other dependencies

--- scripts/dockerbuild-arm64.sh
+++ scripts/dockerbuild-arm64.sh
@@ -10,4 +10,5 @@ echo $DOCKERFILE
 # https://docs.docker.com/build/install-buildx/
 # add this to log the output of the docker build to a file: &> docker-build.log
 # add --no-cache flag to rebuild without using previous builds
-docker build --platform linux/arm64 --network=host --progress=plain -t $CONTAINER_NAME -f $DOCKERFILE .
+# use --progress=plain to see all output in terminal when running a dockerbuild
+docker build --platform linux/arm64 --network=host -t $CONTAINER_NAME -f $DOCKERFILE .
