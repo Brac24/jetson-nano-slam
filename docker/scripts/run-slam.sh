1059099f01ee129c19c68b7185089ed066d9f401
brac24-UbuntuHome <2brac4@gmail.com>
2023-07-27 23:11:03 -0700

    fix renamed realsense script for using aligned depth and also use ./ on the script since they now exist in a directory and not on the root of the filesystem

--- docker/scripts/run-slam.sh
+++ docker/scripts/run-slam.sh
@@ -2,4 +2,4 @@
 
 # Add commands or call other bash scripts that will start nodes we need to run SLAM with rtabmap
 # Use the GNU Parallel utility to run launch file for realsense and rtabmap in parallel
-parallel -u ::: '/launch-legacy-realsense-ros.sh' '/launch-new-rtabmap.sh'
+parallel -u ::: './launch-legacy-realsense-aligned-depth.sh' './launch-new-rtabmap.sh'
